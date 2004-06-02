4fb87ec3b6820afc0937470df420b2501589e712
Frank E Harrell Jr <f.harrell@vanderbilt.edu>
2004-06-02 00:00:00 +0000

    version 2.2-0

--- R/AFirst.lib.s
+++ R/AFirst.lib.s
@@ -1,3 +1,4 @@
+## $Id: AFirst.lib.s,v 1.2 2004/06/01 02:30:59 harrelfe Exp $
 under.unix   <- !(version$os=='Microsoft Windows' ||
                   version$os=='Win32' || version$os=='mingw32')
 
@@ -5,6 +6,9 @@ under.unix   <- !(version$os=='Microsoft Windows' ||
 .SV4.        <- FALSE
 
 .noGenenerics <- TRUE  # faster loading as new methods not used
+
+if(!exists('existsFunction')) existsFunction <- function(...)
+  exists(..., mode='function')
 
 .First.lib <- function(lib, pkg, verbose=TRUE, ...) {
   if(verbose)
