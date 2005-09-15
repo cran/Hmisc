ef5a4e5306fd43d1b0be596d27accf16ada586f9
Charles Dupont <charles.dupont@vanderbilt.edu>
2005-09-15 00:00:00 +0000

    version 3.0-7

--- R/AFirst.lib.s
+++ R/AFirst.lib.s
@@ -1,23 +1,24 @@
-## $Id: AFirst.lib.s,v 1.4 2004/11/21 15:46:28 harrelfe Exp $
-under.unix   <- !(version$os=='Microsoft Windows' ||
-                  version$os=='Win32' || version$os=='mingw32')
+## $Id: AFirst.lib.s,v 1.5 2005/07/12 22:01:34 dupontct Exp $
+under.unix <- !(version$os=='Microsoft Windows' ||
+                version$os=='Win32' || version$os=='mingw32')
 
-.R.          <- TRUE
-.SV4.        <- FALSE
+.R.   <- TRUE
+.SV4. <- FALSE
 
 .noGenenerics <- TRUE  # faster loading as new methods not used
 
 if(!exists('existsFunction')) existsFunction <- function(...)
   exists(..., mode='function')
 
-.First.lib <- function(lib, pkg, ...) {
+.First.lib <- function(lib, pkg, ...)
+{
   verbose <- .Options$Hverbose
   if(!length(verbose) || verbose)
     cat("Hmisc library by Frank E Harrell Jr\n\n",
         "Type library(help='Hmisc'), ?Overview, or ?Hmisc.Overview')\n",
         "to see overall documentation.\n\n",
-		"NOTE:Hmisc no longer redefines [.factor to drop unused levels when\n",
-		"subsetting.  To get the old behavior of Hmisc type dropUnusedLevels().\n",
+        "NOTE:Hmisc no longer redefines [.factor to drop unused levels when\n",
+        "subsetting.  To get the old behavior of Hmisc type dropUnusedLevels().\n",
         sep='')
   library.dynam("Hmisc", pkg, lib)
   invisible()
